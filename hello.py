893ce2512d20f742d4e20fe639a9a3221d2742ec
rncampos <ricardo.campos@ipt.pt>
2023-12-14 10:06:50 +0000

    First Commit

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
-def say_hi():
-    print('Hi, my name is Ricardo Campos.')
+def say_hi(name):
+    print(f'Hi, my name is {name}.')
 
-def city():
-	print("Abrantes")
+def city(name="Covilhã"):
+	print(f"Eu vivo na {name}")
 
 __version__ = '0.1'
